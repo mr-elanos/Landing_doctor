cc928880936ec6631469583eb7a43a1415308f28
Oleksii <moody2mood@gmail.com>
2023-01-13 13:29:34 +0200

    burger menu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -41,9 +41,17 @@ educationModalBtns.forEach(btn =>{
 //burger menu
 let menuBtn = document.querySelector('.header__burger-btn');
 let menu = document.querySelector('.header__burger-menu');
+let navBtns = document.querySelectorAll('.header__burger-li')
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('header__burger-btn-active');
 	menu.classList.toggle('header__burger-menu-active');
     body.classList.toggle('scroll__hidden');
+})
+navBtns.forEach(btn =>{
+    btn.addEventListener('click', function(e){
+    menuBtn.classList.remove('header__burger-btn-active');
+	menu.classList.remove('header__burger-menu-active');
+    body.classList.remove('scroll__hidden');
+    })
 })
